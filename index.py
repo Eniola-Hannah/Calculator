2a60715372d987147e5bd36d77cd6c6e618ba70a
Eniola Hannah <heniola395@gmail.com>
2023-04-09 08:36:20 +0100

    I created function menu(), the first function to be called at the initialization

--- index.py
+++ index.py
@@ -3,6 +3,17 @@ import time
 
 class Cal:
     def __init__(self):
-        self.greetings = "Welcome to Ana's calculator"
-        self.about = "here, ur input will be evaluated correctly"
+        print("Welcome to Ana's calculator \n here, ur input will be evaluated correctly")
+        time.sleep(1)
         self.menu()
+
+    def menu():
+        user = input("""
+                kindly choose your operands
+                
+                    1. Addition
+                    2. Subtraction
+                    3. Multiplication
+                    4. Division
+                    0. Quit
+        """)
