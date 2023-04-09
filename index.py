072901a2a908f173e9fa0b6512085b8751ffefe6
Eniola Hannah <heniola395@gmail.com>
2023-04-09 08:31:41 +0100

    I created a class and input my  intialisations in it with the first function to call which is yet to be created

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import sys
 import time
 
-
-    
+class Cal:
+    def __init__(self):
+        self.greetings = "Welcome to Ana's calculator"
+        self.about = "here, ur input will be evaluated correctly"
+        self.menu()
